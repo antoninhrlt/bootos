65e8b0a8c330af4bc49539e6bda71c9031372e3f
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-28 15:32:32 +0200

    Simple kernel test

---
+++ test/kernel.c
@@ -0,0 +1,10 @@
+// This file is part of "bootos"
+// Under the MIT License
+// Copyright (c) 2022 Antonin Hérault
+
+/**
+ * Loaded by "bootos" at 0x1000
+ * */
+void _start() {
+    for (;;) {}
+}
